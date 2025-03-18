d58b449dbb831098e746ff2c702ca93485dbebf3
Recovery-0ne <scgillesp@gmail.com>
2025-03-18 08:11:27 -0400

    Updated Skull Wolf
    - Skull wolf now uses an animation player

--- spellcast/Enemy/EnemyScripts/SkullWolf/skull_wolf.gd
+++ spellcast/Enemy/EnemyScripts/SkullWolf/skull_wolf.gd
@@ -9,9 +9,9 @@ func stop():
 func change_facing_direction():
 	if velocity.x > 0:
 		facing_dir = 1
-		animated_sprite.flip_h = true
+		sprite.flip_h = true
 		attack_check.position.x = abs(attack_check.position.x)
 	elif  velocity.x < 0:
 		facing_dir = -1
-		animated_sprite.flip_h = false
+		sprite.flip_h = false
 		attack_check.position.x = -abs(attack_check.position.x)
