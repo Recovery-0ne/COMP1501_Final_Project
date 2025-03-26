3f1f818437674136a7090d72a14dd0045c23698e
Recovery-0ne <scgillesp@gmail.com>
2025-03-26 11:09:37 -0400

    Updated ability icon templates
    Removed progress bar functions from the ability icon template since they're only used in the actual slots

---
+++ spellcast/UI/AbilityCooldowns/ability_cooldown_template.gd
@@ -0,0 +1,7 @@
+extends Node2D
+@onready var color = $ColorRect.color
+@onready var sprite_transform = $Sprite2D.transform
+@onready var texture = $Sprite2D.texture
+@onready var hframes = $Sprite2D.hframes
+@onready var vframes = $Sprite2D.vframes
+@onready var frame = $Sprite2D.frame
