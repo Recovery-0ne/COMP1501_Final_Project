3932994aff02c277de7fc234633e19e62ce092ae
Recovery-0ne <scgillesp@gmail.com>
2025-03-31 19:08:14 -0400

    Chest fixed

--- spellcast/Breakables/breakable_chest.gd
+++ spellcast/Breakables/breakable_chest.gd
@@ -3,9 +3,7 @@ extends Node2D
 @export var chest_money = 10
 
 func open_chest():
-	var ui = get_tree().get_first_node_in_group("In_Game_UI")
-	ui.find_child("PlayerMoney").text = "Money: "+str(int(ui.find_child("PlayerMoney").text) + 10)
-	ui = get_tree().get_first_node_in_group("Shop_UI")
-	ui.find_child("PlayerMoney").text = "Money: "+str(int(ui.find_child("PlayerMoney").text) + 10)
+	var player = get_tree().get_first_node_in_group("Player")
+	player.set_money(player.player_money + chest_money)
 	$ChestImage.hide() 
 	$Area2D/CollisionShape2D.disabled = true
