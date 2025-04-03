dca32945734d9bbcf59edc92790edceee0bec11b
Recovery-0ne <scgillesp@gmail.com>
2025-04-03 19:16:26 -0400

    Boss music methods

--- spellcast/Enemy/boss_area.gd
+++ spellcast/Enemy/boss_area.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @export var boss: Enemy
+@export var music_player: Node
 
 func _on_body_entered(body: Node2D) -> void:
-	pass
+	music_player.play_boss_music()
 
 func _on_body_exited(body: Node2D) -> void:
 	boss.reset()
+	music_player.play_default_music()
